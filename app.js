1aacba66c4ca7632ac4a98b75ceb09fc88b798c5
Mike Hong <solitary945@gmail.com>
2022-02-13 00:50:08 +0800

    Add UI interactive Button

--- app.js
+++ app.js
@@ -2,63 +2,93 @@ let computerSelection = computerPlay();
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
+let counter = 0;
+
+const buttons = document.querySelectorAll('button')
+const message = document.querySelector('.message')
+const resultContent = document.querySelector('.result')
+
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', playRound)
+});
+
+function playRound(e) {
+
+    
+    let computerChoice = document.querySelector('.computer')
+    computerSelection = computerPlay()
+    computerChoice.innerHTML = computerSelection
+    playerSelection = e.target.innerHTML 
+    counter += 1
+
+    runGameLogic(playerSelection, computerSelection)
+
+    if (counter !== 5) return;
+
+    const result = document.createElement('p')
+    const winner = document.createElement('p')
+    result.innerHTML = `Game over! Your Score is ${playerScore} vs Computer Score ${computerScore}`
+    winner.innerHTML = (playerScore > computerScore) ? "You are the WINNER 🎉🥳🎉🥳" : "Computer is the WINNER!! 😢😢"
+    resultContent.append(result, winner)
+        
+}
+
+
+
+function runGameLogic(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        message.innerHTML = "It's a tie!"
+    } else if (playerSelection === "✊🏼" && computerSelection === "✋🏼") {
+        computerScore += 1
+        message.innerHTML = "You Lose! ✊🏼 loses to ✋🏼"
+    } else if (playerSelection === "✊🏼" && computerSelection === "✌🏼") {
+        playerScore += 1
+        message.innerHTML = "You Win! ✊🏼 beats ✌🏼"
+    } else if (playerSelection === "✋🏼" && computerSelection === "✊🏼") {
+        playerScore += 1
+        message.innerHTML = "You Win! ✋🏼 beats ✊🏼"
+    } else if (playerSelection === "✋🏼" && computerSelection === "✌🏼") {
+        computerScore += 1
+        message.innerHTML = "You Lose! ✋🏼 Loses to ✌🏼"
+    } else if (playerSelection === "✌🏼" && computerSelection === "✊🏼") {
+        computerScore += 1
+        message.innerHTML = "You Lose! ✋🏼 Loses to ✊🏼"
+    } else if (playerSelection === "✌🏼" && computerSelection === "✋🏼") {
+        playerScore += 1
+        message.innerHTML = "You Win! ✌🏼 beats ✋🏼"
+    } 
+}
+
 
 function computerPlay() {
     ranNum = Math.floor(Math.random() * 3)
     switch (ranNum) {
         case 0:
-        return "Rock";
+        return "✊🏼";
         break;
         case 1:
-        return "Paper";
+        return "✋🏼";
         break;
         case 2:
-        return "Scissor";
+        return "✌🏼";
         break;
     }
     
 }
 
 
-function playRound(playerSelection, computerSelection)  {
-    
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    console.log(playerSelection)
-    if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        computerScore += 1
-        return "You Lose! Rock loses to Paper"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
-        playerScore += 1
-        return "You Win! Rock beats Scissor"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        playerScore += 1
-        return "You Win! Paper beats Rock"
-    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
-        computerScore += 1
-        return "You Lose! Paper Loses to Scissor"
-    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
-        computerScore += 1
-        return "You Lose! Paper Loses to Rock"
-    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
-        playerScore += 1
-        return "You Win! Scissor beats Paper"
-    } 
-        
-    }
 
-for (let i = 0; i < 5; i++) {
-    computerSelection = computerPlay()
-    playerSelection = (prompt("PLease choose your move: ")).toLowerCase();
-    if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissor") {
-        console.log("Please Enter a valid input")
-    } else {
-        console.log(playRound(playerSelection,computerSelection))
-        console.log("player: " + playerSelection + ", computer: " + computerSelection)
-        console.log("Player scores: "+ playerScore)
-        console.log("computer scores: "+ computerScore)
-    }
-    
-}
+
+//     if (playerSelection !== "✊🏼" || playerSelection !== "✋🏼" || playerSelection !== "✌🏼") {
+//         console.log("Please Enter a valid input")
+//     } else {
+//         console.log(playRound(playerSelection,computerSelection))
+//         console.log("player: " + playerSelection + ", computer: " + computerSelection)
+//         console.log("Player scores: "+ playerScore)
+//         console.log("computer scores: "+ computerScore)
+//      
+// 
 
